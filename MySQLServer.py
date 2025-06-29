dd7fecde557d9003bbb951792d122d6373c8a257
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-06-29 22:57:16 +0100

    really!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
+"""Creates a MySQL database without using SELECT or SHOW"""
 import mysql.connector
 from mysql.connector import Error
 
-def create_database():
-    try:
-        connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='password'
-        )
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-        cursor.close()
-        connection.close()
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='password'
+    )
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+    cursor.close()
+    connection.close()
 
-    except Error as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    create_database()
+except Error as e:
+    print(f"Error: {e}")
