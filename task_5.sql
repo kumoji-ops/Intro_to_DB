da510f621a0a50c55be546ab3358b38f01fafa01
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-06-29 23:54:57 +0100

    lll

--- task_5.sql
+++ task_5.sql
@@ -1,14 +1,3 @@
- - `customer_id` = `2`
-    - `customer_name` = `Blessing Malik`
-    - `email` = `bmalik@sandtech.com`
-    - `address` = `124 Happiness  Ave.`
-    - `customer_id` = `3`
-    - `customer_name` = `Obed Ehoneah`
-    - `email` = `eobed@sandtech.com`
-    - `address` = `125 Happiness  Ave.`
-    - `customer_id` = `4`
-    - `customer_name` = `Nehemial Kamolu`
-    - `email` = `nkamolu@sandtech.com`
-    - `address` = `126 Happiness  Ave.`
+
 INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
