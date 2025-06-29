b19f3901a841c65441972122f2190211dc517919
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-06-29 23:37:29 +0100

    lll

--- task_4.sql
+++ task_4.sql
@@ -43,6 +43,50 @@ CREATE TABLE Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+-- Use the existing database
+USE alx_book_store;
+
+-- Create AUTHORS table
+CREATE TABLE IF NOT EXISTS AUTHORS (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create BOOKS table
+CREATE TABLE IF NOT EXISTS BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- Create CUSTOMERS table
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- Create ORDERS table
+CREATE TABLE IF NOT EXISTS ORDERS (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+-- Create ORDER_DETAILS table
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
 SELECT
     COLUMN_NAME AS `Field`,
     COLUMN_TYPE AS `Type`,
@@ -57,4 +101,3 @@ WHERE
     AND TABLE_NAME = 'books'
 ORDER BY
     ORDINAL_POSITION;
-
